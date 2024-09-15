866f635a9b8ae20d6cd2a8aa9a43e245d290d078
Daniele Conti <fourlastor@users.noreply.github.com>
2024-09-15 15:27:57 +0200

    Show background

--- core/src/main/java/io/github/fourlastor/game/intro/IntroScreen.java
+++ core/src/main/java/io/github/fourlastor/game/intro/IntroScreen.java
@@ -18,20 +18,26 @@ import space.earlygrey.shapedrawer.scene2d.ShapeDrawerDrawable;
 
 public class IntroScreen extends ScreenAdapter {
 
-    public static Color CLEAR_COLOR = new Color(0x333333ff);
+    private static final Color CLEAR_COLOR = new Color(0x222222ff);
+    private static final int TILE_SIZE = 32;
+    private static final int TILE_COUNT = 10;
 
     private final Stage stage;
     private final Viewport viewport;
 
     @Inject
     public IntroScreen(@Named(WHITE_PIXEL) TextureRegion whitePixel) {
-        viewport = new FitViewport(256, 144);
+        viewport = new FitViewport(TILE_SIZE * TILE_COUNT, TILE_SIZE * (TILE_COUNT + 3));
         stage = new Stage(viewport);
         ShapeDrawer shapeDrawer = new ShapeDrawer(stage.getBatch(), whitePixel);
+        Image bg = new Image(whitePixel);
+        bg.setSize(stage.getWidth(), stage.getHeight());
+        bg.setColor(new Color(0x333333ff));
+        stage.addActor(bg);
         Image image = new Image(new ShapeDrawerDrawable(shapeDrawer) {
             @Override
             public void drawShapes(ShapeDrawer shapeDrawer, float x, float y, float width, float height) {
-                int count = 12;
+                int count = TILE_COUNT;
                 float squareWidth = width / count;
                 float squareHeight = height / count;
                 boolean dark = true;
@@ -47,8 +53,8 @@ public class IntroScreen extends ScreenAdapter {
                 }
             }
         });
-        image.setSize(144, 144);
-        image.setPosition(stage.getWidth() / 2, stage.getHeight() / 2, Align.center);
+        image.setSize(TILE_SIZE * TILE_COUNT, TILE_SIZE * TILE_COUNT);
+        image.setPosition(stage.getWidth() / 2, 0, Align.center | Align.bottom);
         stage.addActor(image);
     }
 
