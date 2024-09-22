e1b1f1fcda6d9a4d6913b92ee7ed6141c265154f
Sandra Moen <sandramoen01@gmail.com>
2024-09-22 10:14:37 +0200

    Minor html launcher fixes + preloadlogo.png

--- html/src/main/java/io/github/fourlastor/game/client/HtmlLauncher.java
+++ html/src/main/java/io/github/fourlastor/game/client/HtmlLauncher.java
@@ -19,6 +19,19 @@ public class HtmlLauncher extends GwtApplication {
         }
 
         @Override
+        public Preloader.PreloaderCallback getPreloaderCallback() {
+                return createPreloaderPanel(GWT.getHostPageBaseURL() + "preloadlogo.png");
+        }
+
+        @Override
+        protected void adjustMeterPanel(Panel meterPanel, Style meterStyle) {
+                meterPanel.setStyleName("gdx-meter");
+                meterPanel.addStyleName("nostripes");
+                meterStyle.setProperty("backgroundColor", "#281828");
+                meterStyle.setProperty("backgroundImage", "none");
+        }
+
+        @Override
         public ApplicationListener createApplicationListener () {
                 return GdxGame.createGame();
         }
